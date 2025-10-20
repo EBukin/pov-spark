15bee5143985ed42cd0513c0d3149c0e40ac4eb1
Eduard Bukin <edwardbukin@gmail.com>
2025-10-20 17:33:27 -0400

    Testing gini random access and FGT

--- R/ftg.R
+++ R/ftg.R
@@ -1,4 +1,30 @@
 
+fgt_collapse <- function(
+  welfare,
+  weight = rep(1, length(welfare)),
+  povline = collapse::fmedian(welfare, w = weight)/2,
+  alpha = 0,
+  ...
+) {
+  pov_status <- welfare < povline
+  relative_distance <- 1 - (welfare / povline)
+  collapse::fmean((pov_status) * (relative_distance)^alpha, w = weight)
+}
+
+
+fgt_base <- function(
+  welfare,
+  weight = rep(1, length(welfare)),
+  povline = collapse::fmedian(welfare, w = weight)/2,
+  alpha = 0,
+  ...
+) {
+  pov_status <- welfare < povline
+  relative_distance <- 1 - (welfare / povline)
+  sum(((pov_status) * (relative_distance)^alpha) * weight) / sum(weight)
+}
+
+
 
 get_pov_slow__dplyr <- function(dta, pl) {
   dta |> 
--- vignettes/00-master.R
+++ vignettes/00-master.R
@@ -1,6 +1,16 @@
 library(quarto)
 
 quarto_render(
+  input = "vignettes/08-random-access.qmd",
+  output_format = "html",
+  execute_params = list(),
+  quiet = FALSE
+)
+
+
+
+
+quarto_render(
   input = "vignettes/10-gini-performance.qmd",
   output_format = "html",
   execute_params = list(),
