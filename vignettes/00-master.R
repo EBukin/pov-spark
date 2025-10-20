48d43e3d4015ec60080901ac5c62685452fcd12b
Eduard Bukin <edwardbukin@gmail.com>
2025-10-20 15:26:11 -0400

    Benchmarking Gini calculations

---
+++ vignettes/00-master.R
@@ -0,0 +1,22 @@
+library(quarto)
+
+quarto_render(
+  input = "vignettes/10-gini-performance.qmd",
+  output_format = "html",
+  execute_params = list(),
+  quiet = FALSE
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
